c5389d58293918f574324786e93ad5788f6e7008
icowen <ian_cowen@yahoo.com>
2019-12-16 09:45:38 -0500

    Initial commit

---
+++ net.js
@@ -0,0 +1,46 @@
+const fetch = require("file-fetch");
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
+async function model() {
+    let x_train = await fetch('input_notes.json').then(res => res.json());
+    let y_train = await fetch('output_notes.json').then(res => res.json());
+    let x = [];
+    for (let i in x_train) {
+        x.push(x_train[i])
+    }
+    let y = [];
+    for (let i in y_train) {
+        y.push(y_train[i])
+    }
+
+    const model = tf.sequential({
+        layers: [
+            // tf.layers.flatten({units: 1, inputShape: [256]}),
+            tf.layers.dense({inputShape: [256], units: 100, activation: 'sigmoid'}),
+            tf.layers.dense({units: 128, activation: 'sigmoid'})
+        ]
+    });
+    model.compile({
+        optimizer: 'adam',
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    });
+    model.fit(tf.tensor2d(x), tf.tensor2d(y), {
+        epochs: 10000,
+        validationSplit: .8,
+        callbacks: {onBatchEnd}
+    }).then(info => {
+        console.log('Final accuracy', info.history.acc);
+    });
+
+    return model;
+}
+
+function onBatchEnd(batch, logs) {
+    console.log('Accuracy', logs.acc);
+}
+
+model()
+    .then(model => model.save('file://./model-1b'))
+    .then(res => console.error('res:', res));
